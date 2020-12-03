7a3d6a1888343eb994400cd74c0c0586747b1d9f
ujwal-gautam <ujwal-gautam@github.com>
2020-12-03 12:33:13 +0530

    initial commit

---
+++ src/main/java/com/demo/ServletDemo.java
@@ -0,0 +1,34 @@
+package com.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class ServletDemo extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/plain");
+		resp.getWriter().write("Hello World! Maven Web Project Example.");
+		/*
+		 * PrintWriter printWriter = resp.getWriter();
+		 * printWriter.print("<h1>Hello</h1>");
+		 */
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPost(req, resp);
+	}
+
+}
